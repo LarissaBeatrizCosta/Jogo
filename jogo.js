afe5981b4efa64b42229c90eb82bfcc904cae166
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2023-12-15 11:01:32 -0300

    finalizado jogo

--- jogo.js
+++ jogo.js
@@ -1,4 +1,6 @@
 const princess = document.querySelector("#princess");
+const flower = document.querySelector("#flower");
+
 
 
 const jump = () => {
@@ -9,5 +11,27 @@ const jump = () => {
     }, 500);
 
 }
+
+const loop= setInterval(()=>{
+
+    const flowerPosition = flower.offsetLeft;
+    const princessPosition = +window.getComputedStyle(princess).bottom.replace("px", "");
+
+    if(flowerPosition <=80 && princessPosition <60){
+        flower.style.animation = "none";
+        flower.style.left = `${flowerPosition}px`;
+        
+        princess.style.animation = "none";
+        princess.style.left = `${princessPosition}px`;
+
+   
+    
+        clearInterval(loop);
+    }
+    
+},10)
+
+
+
 
 document.addEventListener("keydown", jump);
